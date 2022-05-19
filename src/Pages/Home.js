e94540cb990bcdf830e2e7ab05e613784a100ab0
reddyarun59 <reddyarun59@yahoo.com>
2022-05-19 18:38:32 +0530

    removed home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
 
 const Home = (props) => {
-  const history = useNavigate();
-  history("/quiz")
+   
   return (
     <div className="flex flex-col justify-center h-screen">
         <h1 className="mx-auto font-karla text-3xl">Quizzical</h1>
